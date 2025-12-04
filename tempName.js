2220ce1d57da83b39239b4ed8e5727525cf3b413
Alpaca3245 <kyle.luu11@gmail.com>
2025-12-04 12:34:11 -0500

    Added guideSectionRenderer

--- tempName.js
+++ tempName.js
@@ -1,21 +1,22 @@
 // Elements
-const guideSection = createElement(ytd-guide-section-renderer);
-
+const guideSectionRenderer = document.createElement('ytd-guide-section-renderer');
+guideSectionRenderer.className = "style-scope ytd-guide-renderer";
 
 
 // Observer
-const target = document;
-const config = {childList: true, subtree: true};
-observer.observe(target, config);
-
 const observer = new MutationObserver(function () {
-
       console.log("mutation fired");
       console.log("sections exists:", !!document.getElementById('sections'));
 
       if (document.getElementById('sections')) {
-            observer.disconnect(); // Stop observing now that #sections exists to prevent further mutation triggers]
+            observer.disconnect(); // Stop observing now that #sections exists to prevent further mutation triggers
+            const sections = document.getElementById('sections');
+            sections.appendChild(guideSectionRenderer);
+
             console.log("appending");
       }
 });
 
+const target = document;
+const config = {childList: true, subtree: true};
+observer.observe(target, config);
