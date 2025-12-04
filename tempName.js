600cb7d777a0ba5554f50b9789ff288635996115
Alpaca3245 <kyle.luu11@gmail.com>
2025-12-04 13:07:41 -0500

    Fixed observer from firing more than once using insert variable, and added children after the subscriber element

--- tempName.js
+++ tempName.js
@@ -2,18 +2,21 @@
 const guideSectionRenderer = document.createElement('ytd-guide-section-renderer');
 guideSectionRenderer.className = "style-scope ytd-guide-renderer";
 
+// Variable
+const inserted = false;
 
 // Observer
 const observer = new MutationObserver(function () {
       console.log("mutation fired");
-      console.log("sections exists:", !!document.getElementById('sections'));
+      console.log("sections children exists:", !!document.getElementById('sections').children[5]);
 
-      if (document.getElementById('sections')) {
+      if (document.getElementById('sections').children[5] && inserted == false) {
             observer.disconnect(); // Stop observing now that #sections exists to prevent further mutation triggers
             const sections = document.getElementById('sections');
-            sections.appendChild(guideSectionRenderer);
-
+            console.log(sections.children[2]);
+            sections.insertBefore(guideSectionRenderer, sections.children[2]);
             console.log("appending");
+            inserted = true; // to prevent observer from firing more than once
       }
 });
 
